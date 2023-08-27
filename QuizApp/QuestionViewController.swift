8f2407f24985db97e31d1ee0cb257c4e85f7df7a
mfsaglam <mfsaglam1@gmail.com>
2023-08-27 16:19:16 +0300

    End of Tomato 5 - Implementing some basic UI

---
+++ QuizApp/QuestionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Fatih Sağlam on 27.08.2023.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var question: String = ""
+    private var options: [String] = []
+    
+    convenience init(question: String, options: [String]) {
+        self.init()
+        self.question = question
+        self.options = options
+    }
+    
+    override func viewDidLoad() {
+        headerLabel.text = question
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+}
---
+++ QuizAppTests/QuestionViewControllerTests.swift
@@ -0,0 +1,46 @@
+//
+//  QuestionViewControllerTests.swift
+//  QuizAppTests
+//
+//  Created by Fatih Sağlam on 27.08.2023.
+//
+
+import XCTest
+@testable import QuizApp
+
+class QuestionViewControllerTests: XCTestCase {
+    
+    func test_viewDidLoad_rendersHeaderText() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.headerLabel.text, "Q1")
+    }
+    
+    func test_viewDidLoad__withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad__withOneOption_rendersOneOption() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    func test_viewDidLoad__withOneOption_rendersOneOptionText() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        _ = sut.view
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
+        XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+}
