208308cb29f376b4ef0e1f6e178b199fa5ce2bdb
mfsaglam <mfsaglam1@gmail.com>
2023-09-01 19:05:33 +0300

    Finish Game (startGame) implementation

---
+++ Engine/QuizEngineTests/GameTest.swift
@@ -0,0 +1,39 @@
+//
+//  GameTest.swift
+//  QuizEngineTests
+//
+//  Created by Fatih Sağlam on 1.09.2023.
+//
+
+import XCTest
+import QuizEngine
+
+class GameTest: XCTestCase {
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+    
+    override func setUp() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1","Q2": "A2"])
+    }
+    
+    func test_startGame_answerZeroOutOfTwoCorrect_scoresZero() {
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+    
+    func test_startGame_answerOneOutOfTwoCorrect_scoresOne() {
+        router.answerCallback("A1")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 1)
+    }
+    
+    func test_startGame_answerTwoOutOfTwoCorrect_scoresTwo() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResult!.score, 2)
+    }
+}
