9d99c41f154500e1a90fcce169ea1fab11cb8093
mfsaglam <mfsaglam1@gmail.com>
2023-09-02 14:00:18 +0300

    End of Tomato 12 - NavigationControllerRouter

---
+++ App/QuizApp/NavigationControllerRouter.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationControllerRouter.swift
+//  QuizApp
+//
+//  Created by Fatih Sağlam on 2.09.2023.
+//
+
+import UIKit
+import QuizEngine
+
+class NavigationControllerRouter: Router {
+    private let navigationController: UINavigationController
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        navigationController.pushViewController(UIViewController(), animated: false)
+    }
+    
+    func routeTo(result: QuizEngine.Result<String, String>) {
+        
+    }
+}
---
+++ App/QuizAppTests/NavigationControllerRouterTests.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationControllerRouterTests.swift
+//  QuizAppTests
+//
+//  Created by Fatih Sağlam on 1.09.2023.
+//
+
+import XCTest
+import QuizEngine
+@testable import QuizApp
+
+class NavigationControllerRouterTests: XCTestCase {
+    
+    func test_routeToQuestion_presentsQuestionController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+        
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+    }
+    
+    func test_routeToQuestionTwice_presentsQuestionController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+        
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        sut.routeTo(question: "Q2", answerCallback: { _ in })
+
+        XCTAssertEqual(navigationController.viewControllers.count, 2)
+    }
+}
