3e81ae5ccd636fbe67283ff67381c410cd1b3ed5
mfsaglam <mfsaglam1@gmail.com>
2023-09-02 17:35:33 +0300

    move Question into its own file

---
+++ App/QuizApp/Question.swift
@@ -0,0 +1,33 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Fatih Sağlam on 2.09.2023.
+//
+
+import Foundation
+
+enum Question<T: Hashable>: Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .singleAnswer(let value):
+            hasher.combine(value)
+        case .multipleAnswer(let value):
+            hasher.combine(value)
+        }
+    }
+    
+    static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        case (.multipleAnswer(let a), .multipleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
+    }
+}
