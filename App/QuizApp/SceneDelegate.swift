994d217b72169bac2a9994ad6f58c37100ef1dc5
mfsaglam <mfsaglam1@gmail.com>
2023-10-29 15:08:16 +0300

    End of Tomato 21 - Putting everything together and running the app for the first time!

--- App/QuizApp/SceneDelegate.swift
+++ App/QuizApp/SceneDelegate.swift
@@ -6,28 +6,41 @@
 //
 
 import UIKit
+import QuizEngine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var game: Game<Question<String>, Set<String>, NavigationControllerRouter>?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        let viewController = ResultsViewController(summary: "You got 1/2 correct", answers: [
-            PresentableAnswer(question: "Question? Question? Question? Question? Question? Question? ", answer: "Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! Yeah! ", wrongAnswer: nil),
-            PresentableAnswer(question: "Question2? Question2? Question2? Question2? Question2? Question2? Question2? ", answer: "Hell yeah!", wrongAnswer: "Hell no! Hell no! Hell no! Hell no! Hell no! Hell no! ")
-        ])
-        _ = viewController.view
-//        viewController.tableView.allowsMultipleSelection = true
-        window?.rootViewController = viewController
-
+        
+        let question1 = Question.singleAnswer("What's Mike's nationality?")
+        let question2 = Question.multipleAnswer("What are Caio's nationalities?")
+        let questions = [question1, question2]
+        
+        let option1 = "Canadian"
+        let option2 = "American"
+        let option3 = "Greek"
+        let options1 = [option1, option2, option3]
+        
+        let option4 = "Portuguese"
+        let option5 = "American"
+        let option6 = "Brazilian"
+        let options2 = [option4, option5, option6]
+        
+        let correctAnswers = [question1: Set([option3]), question2: Set([option4, option6])]
+        
+        let navigationController = UINavigationController()
+        let factory = iOSViewControllerFactory(questions: questions, options: [question1: options1, question2: options2], correctAnswers: correctAnswers)
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
+        game = startGame(questions: questions, router: router, correctAnswers: correctAnswers)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
