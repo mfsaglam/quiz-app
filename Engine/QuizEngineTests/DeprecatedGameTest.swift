6514600fd307e6bced535261d23edd96964646df
mfsaglam <mfsaglam1@gmail.com>
2023-11-04 13:17:26 +0300

    Replicated the RouterSpy helper in a private scope within the deprecated tests so we don't forget to remove it when it's time

--- Engine/QuizEngineTests/DeprecatedGameTest.swift
+++ Engine/QuizEngineTests/DeprecatedGameTest.swift
@@ -10,8 +10,8 @@ import QuizEngine
 
 @available(*, deprecated)
 class DeprecatedGameTest: XCTestCase {
-    let router = RouterSpy()
-    var game: Game<String, String, RouterSpy>!
+    private let router = RouterSpy()
+    private var game: Game<String, String, RouterSpy>!
     
     override func setUp() {
         game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1","Q2": "A2"])
@@ -36,5 +36,18 @@ class DeprecatedGameTest: XCTestCase {
         router.answerCallback("A2")
         
         XCTAssertEqual(router.routedResult!.score, 2)
+    }
+    
+    private class RouterSpy: Router {
+        var routedResult: Result<String, String>? = nil
+        var answerCallback: ((String) -> Void) = { _ in }
+        
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            self.answerCallback = answerCallback
+        }
+        
+        func routeTo(result: Result<String, String>) {
+            routedResult = result
+        }
     }
 }
