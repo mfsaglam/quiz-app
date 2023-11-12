0a02d237622792996319c915fa5c86e094bafd50
mfsaglam <mfsaglam1@gmail.com>
2023-11-12 20:01:11 +0300

    add new todo for removal of the hashable constraint for the `Question` and make the `Result` type generic

--- Engine/QuizEngine/Router.swift
+++ Engine/QuizEngine/Router.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
+/// Used by the QuizEngine's Flow to delegate handling the `nextQuestion` and handling the `Result`.
+/// It is called QuizDelegate because clients doesn't know about the `Flow` outside the `Engine`.
+/// it is not called QuizGameDelegate, because clients should decide if they want to Gamify it or just make a survey type of a quiz.
 public protocol QuizDelegate {
+    // TODO: Remove Hashable constraint and make the result type Generic.
     associatedtype Question: Hashable
     associatedtype Answer
     
+    /// thats what it does, it handle's the `Result` and its client's responsibility to handle it.
     func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
-    func handle(result: Result<Question, Answer>)
+    func handle(result: Result<Question, Answer>) /// Result can be an associatedtype too. it is leaking the implementation to the clients. Migrate first, and start changing it.
 }
 
 @available(*, deprecated) // TODO: Add deprecated message
