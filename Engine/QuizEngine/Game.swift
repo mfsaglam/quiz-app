9a96db57c45935e34e28df1e777aca3f8b7e91ed
mfsaglam <mfsaglam1@gmail.com>
2023-11-12 21:34:25 +0300

    Replaced the Flow Router with a QuizDelegate

--- Engine/QuizEngine/Game.swift
+++ Engine/QuizEngine/Game.swift
@@ -9,19 +9,45 @@ import Foundation
 
 /// We deprecate these because everything is `public` might be used by a client. So we need new `api`s.
 @available(*, deprecated) // TODO: Add a deprecated message
-public class Game<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
-    private let flow: Flow<R>
+public class Game<Question: Hashable, Answer, R: Router> {
+    private let flow: Any
 
-    init(flow: Flow<R>) {
+    init(flow: Any) {
         self.flow = flow
     }
 }
 
 @available(*, deprecated) // TODO: Add deprecated message
-public func startGame<Question: Hashable, Answer: Equatable, R: Router>(questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, router: router, scoring: { scoring($0, correctAnswers: correctAnswers ) })
+public func startGame<Question: Hashable, Answer: Equatable, R: Router>(
+    questions: [Question],
+    router: R,
+    correctAnswers: [Question: Answer]
+) -> Game<Question, Answer, R>  where R.Question == Question, R.Answer == Answer {
+    let flow = Flow(questions: questions, router: QuizDelegateToRouterAdapter(router), scoring: { scoring($0, correctAnswers: correctAnswers ) })
     flow.start()
     return Game(flow: flow)
+}
+
+@available(*, deprecated) // Add a deprecated message
+private class QuizDelegateToRouterAdapter<R: Router>: QuizDelegate {
+    private let router: R
+    
+    init(_ router: R) {
+        self.router = router
+    }
+    
+    func handle(
+        question: R.Question,
+        answerCallback: @escaping (R.Answer)-> Void
+    ) {
+        router.routeTo(question: question, answerCallback: answerCallback)
+    }
+    
+    func handle(
+        result: Result<R.Question, R.Answer>
+    ) {
+        router.routeTo(result: result)
+    }
 }
 
 // TODO: What should we do with this function? (it is a leaky detail)
