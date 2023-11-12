785d616f583ba471666166fa0d5319458afd2ad4
mfsaglam <mfsaglam1@gmail.com>
2023-11-12 21:11:53 +0300

    renamed the Helper and variable to clarify new intent

--- Engine/QuizEngineTests/FlowTest.swift
+++ Engine/QuizEngineTests/FlowTest.swift
@@ -107,16 +107,16 @@ class FlowTest: XCTestCase {
     }
     
     //MARK: Helpers
-    private let delegate = RouterSpy()
+    private let delegate = DelegateSpy()
     
     private func makeSUT(
         questions: [String],
         scoring: @escaping ([String: String]) -> Int = { _ in 0 }
-    ) -> Flow<RouterSpy> {
+    ) -> Flow<DelegateSpy> {
         return Flow(questions: questions, router: delegate, scoring: scoring)
     }
     
-    private class RouterSpy: Router {
+    private class DelegateSpy: Router {
         var routedQuestions: [String] = []
         var routedResult: Result<String, String>? = nil
         var answerCallback: ((String) -> Void) = { _ in }
