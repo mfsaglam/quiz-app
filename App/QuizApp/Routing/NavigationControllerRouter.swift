7b8d13f6911d026d8c927cfbf54cdb39a12f364f
mfsaglam <mfsaglam1@gmail.com>
2023-12-05 05:56:51 +0300

    Remove deprecated Router implementation

--- App/QuizApp/Routing/NavigationControllerRouter.swift
+++ App/QuizApp/Routing/NavigationControllerRouter.swift
@@ -8,7 +8,7 @@
 import UIKit
 import QuizEngine
 
-class NavigationControllerRouter: Router, QuizDelegate {
+class NavigationControllerRouter: QuizDelegate {
     private let navigationController: UINavigationController
     private let factory: ViewControllerFactory
     
@@ -32,17 +32,9 @@ class NavigationControllerRouter: Router, QuizDelegate {
             show(controller)
         }
     }
-
-    func routeTo(question: Question<String>, answerCallback: @escaping ([String]) -> Void) {
-        answer(for: question, completion: answerCallback)
-    }
     
     func didCompleteQuiz(withAnswers answers: [(question: QuizEngine.Question<String>, answer: [String])]) {
         show(factory.resultsViewController(for: answers))
-    }
-    
-    func routeTo(result: Result<Question<String>, [String]>) {
-        show(factory.resultsViewController(for: result))
     }
     
     private func show(_ viewController: UIViewController) {
