68d0f650adda83dbfae3741b70b68ea21adc1d7b
mfsaglam <mfsaglam1@gmail.com>
2023-12-05 06:49:00 +0300

    Removed deprecated method

--- App/QuizApp/Main/iOSViewControllerFactory.swift
+++ App/QuizApp/Main/iOSViewControllerFactory.swift
@@ -62,17 +62,4 @@ final class iOSViewControllerFactory: ViewControllerFactory {
         controller.title = presenter.title
         return controller
     }
-    
-    func resultsViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
-        let presenter = ResultsPresenter(
-            userAnswers: questions.map { question in
-                (question, result.answers[question]!)
-            },
-            correctAnswers: correctAnswers.map { $0 },
-            scorer: { _, _ in result.score }
-        )
-        let controller = ResultsViewController(summary: presenter.summary, answers: presenter.presentableAnswers)
-        controller.title = presenter.title
-        return controller
-    }
 }
