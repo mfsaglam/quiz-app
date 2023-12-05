91a0b1abe026c25f301f0856c7c471bd278025da
mfsaglam <mfsaglam1@gmail.com>
2023-12-05 05:16:12 +0300

    Removed a deprecated initializer

--- App/QuizApp/Main/iOSViewControllerFactory.swift
+++ App/QuizApp/Main/iOSViewControllerFactory.swift
@@ -24,20 +24,6 @@ class iOSViewControllerFactory: ViewControllerFactory {
         self.correctAnswers = { correctAnswers }
     }
     
-    // TODO: - Remove Deprecated initializer
-    private init(
-        questions: [Question<String>],
-        options: Dictionary<Question<String>, [String]>,
-        correctAnswers: Dictionary<Question<String>, [String]>
-    ) {
-        self.questions = questions
-        self.options = options
-        self.correctAnswers = { questions.map { question in
-            (question, correctAnswers[question]!)
-        }
-        }
-    }
-    
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
         guard let options = self.options[question] else {
             fatalError("Couldn't find options for question: \(question)")
