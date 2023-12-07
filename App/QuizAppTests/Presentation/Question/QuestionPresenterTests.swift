e9f94c870e3dda504e8a13c32160f7f3c2995156
mfsaglam <mfsaglam1@gmail.com>
2023-12-07 07:05:43 +0300

    Implement new question title format

--- App/QuizAppTests/Presentation/Question/QuestionPresenterTests.swift
+++ App/QuizAppTests/Presentation/Question/QuestionPresenterTests.swift
@@ -5,40 +5,32 @@
 //  Created by Fatih Sağlam on 10.09.2023.
 //
 
+import Foundation
 import XCTest
 import QuizEngine
 @testable import QuizApp
 
-class QuestionPresenterTests: XCTestCase {
+class QuestionPresenterTest: XCTestCase {
     
-    let question1 = Question.singleAnswer("Q1")
-    let question2 = Question.multipleAnswer("Q2")
-    
-    func test_title_forFirstQuestion_formatsTitleForIndex() {
-        let sut = QuestionPresenter(
-            questions: [question1],
-            question: question1
-        )
-        
-        XCTAssertEqual(sut.title, "Question #1")
-    }
-    
-    func test_title_forSecondQuestion_formatsTitleForIndex() {
-        let sut = QuestionPresenter(
-            questions: [question1, question2],
-            question: question2
-        )
-        
-        XCTAssertEqual(sut.title, "Question #2")
-    }
-    
-    func test_title_forUnexistentQuestion_isEmpty() {
+    let question1 = Question.singleAnswer("A1")
+    let question2 = Question.multipleAnswer("A2")
 
-        let sut = QuestionPresenter(
-            questions: [],
-            question: Question.singleAnswer("Q1")
-        )
+    func test_title_forFirstQuestion_formatsTitleForIndex() {
+        let sut = QuestionPresenter(questions: [question1, question2], question: question1)
+        
+        XCTAssertEqual(sut.title, "1 of 2")
+    }
+ 
+    func test_title_forSecondQuestion_formatsTitleForIndex() {
+        let sut = QuestionPresenter(questions: [question1, question2], question: question2)
+        
+        XCTAssertEqual(sut.title, "2 of 2")
+    }
+
+    func test_title_forUnexistentQuestion_isEmpty() {
+        let sut = QuestionPresenter(questions: [], question: Question.singleAnswer("A1"))
         
         XCTAssertEqual(sut.title, "")
     }
+
 }
