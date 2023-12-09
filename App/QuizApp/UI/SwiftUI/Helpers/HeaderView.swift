089b2dbfd22213924b9ed135dfceee90f23fca8f
mfsaglam <mfsaglam1@gmail.com>
2023-12-09 16:20:08 +0300

    Add ResultView

--- App/QuizApp/UI/SwiftUI/Helpers/HeaderView.swift
+++ App/QuizApp/UI/SwiftUI/Helpers/HeaderView.swift
@@ -10,25 +10,29 @@ import SwiftUI
 struct HeaderView: View {
     let title: String
     let subtitle: String
-
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 16.0) {
-            Text(title)
-                .font(.headline)
-                .fontWeight(.medium)
-                .foregroundColor(Color.blue)
-                .padding(.top)
-
-            Text(subtitle)
-                .font(.largeTitle)
-                .fontWeight(.medium)
+        HStack {
+            VStack(alignment: .leading, spacing: 16.0) {
+                Text(title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.blue)
+                    .padding(.top)
+                
+                Text(subtitle)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }
+            
+            Spacer()
         }.padding()
     }
 }
 
-struct HeaderView_Previews: PreviewProvider {
+struct QuestionHeader_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView(title: "A title", subtitle: "A question")
+        HeaderView(title: "A title", subtitle: "A subtitle")
             .previewLayout(.sizeThatFits)
     }
 }
