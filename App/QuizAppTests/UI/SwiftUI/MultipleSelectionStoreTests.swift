563cb121e139adc36e7d7afecd6ad71163456e9e
mfsaglam <mfsaglam1@gmail.com>
2023-12-09 15:39:34 +0300

    Rename method to clarify intent

--- App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -14,10 +14,10 @@ class MultipleSelectionStoreTests: XCTestCase {
         var sut = MultipleSelectionStore(options: ["o0", "o1"])
         XCTAssertFalse(sut.options[0].isSelected)
 
-        sut.options[0].select()
+        sut.options[0].toggleSelection()
         XCTAssertTrue(sut.options[0].isSelected)
 
-        sut.options[0].select()
+        sut.options[0].toggleSelection()
         XCTAssertFalse(sut.options[0].isSelected)
     }
     
@@ -26,13 +26,13 @@ class MultipleSelectionStoreTests: XCTestCase {
         var sut = MultipleSelectionStore(options: ["o0", "o1"])
         XCTAssertFalse(sut.canSubmit)
 
-        sut.options[0].select()
+        sut.options[0].toggleSelection()
         XCTAssertTrue(sut.canSubmit)
 
-        sut.options[0].select()
+        sut.options[0].toggleSelection()
         XCTAssertFalse(sut.canSubmit)
 
-        sut.options[1].select()
+        sut.options[1].toggleSelection()
         XCTAssertTrue(sut.canSubmit)
     }
     
@@ -45,11 +45,11 @@ class MultipleSelectionStoreTests: XCTestCase {
         sut.submit()
         XCTAssertEqual(submittedOptions, [])
         
-        sut.options[0].select()
+        sut.options[0].toggleSelection()
         sut.submit()
         XCTAssertEqual(submittedOptions, [["o0"]])
         
-        sut.options[1].select()
+        sut.options[1].toggleSelection()
         sut.submit()
         XCTAssertEqual(submittedOptions, [["o0"], ["o0", "o1"]])
     }
