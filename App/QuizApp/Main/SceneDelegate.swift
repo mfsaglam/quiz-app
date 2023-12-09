d7cf661d435652ec36c1e45b9da5ab7eaa926205
mfsaglam <mfsaglam1@gmail.com>
2023-12-09 17:21:22 +0300

    Restart Quiz when user taps "Play again"

--- App/QuizApp/Main/SceneDelegate.swift
+++ App/QuizApp/Main/SceneDelegate.swift
@@ -12,10 +12,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var quiz: Quiz?
-
+    
+    private lazy var navigationController = UINavigationController()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        startNewQuiz()
+    }
+    
+    private func startNewQuiz() {
         let question1 = Question.singleAnswer("What's Mike's nationality?")
         let question2 = Question.multipleAnswer("What are Caio's nationalities?")
         let questions = [question1, question2]
@@ -32,15 +43,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let options = [question1: options1, question2: options2]
         let correctAnswers = [(question1, [option3]), (question2, [option4, option6])]
-
-        let navigationController = UINavigationController()
-        let factory = iOSSwiftUIViewControllerFactory(options: options, correctAnswers: correctAnswers)
-
-        let router = NavigationControllerRouter(navigationController, factory: factory)
         
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        let factory = iOSSwiftUIViewControllerFactory(
+            options: options,
+            correctAnswers: correctAnswers,
+            playAgain: startNewQuiz
+        )
+        
+        let router = NavigationControllerRouter(navigationController, factory: factory)
         
         quiz = Quiz.start(questions: questions, delegate: router)
     }
